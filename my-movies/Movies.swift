42acd3b4714479be1b5d109f98be49177e445fa8
Emerson Novais <emersonnovais93@gmail.com>
2022-03-30 19:36:57 -0300

    First commit
    Start creation of app

---
+++ my-movies/Movies.swift
@@ -0,0 +1,19 @@
+//
+//  Movies.swift
+//  my-movies
+//
+//  Created by Emerson.Novais on 30/03/22.
+//
+
+import UIKit
+
+class Movies{
+    var title: String
+    var description: String
+    var image: UIImage
+    
+    init(title: String, description: String) {
+        self.title = title
+        self.description = description
+    }
+}
--- my-movies/ViewController.swift
+++ my-movies/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var movies: [Movies] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        movies.append(Movies(title: "movie1", description: "description1"))
+        movies.append(Movies(title: "movie2", description: "description2"))
     }
 
-
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let movie = movies[indexPath.row]
+        let identifier = "cellReuse"
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = movies[indexPath.row].title
+        return cell
+    }
 }
 
