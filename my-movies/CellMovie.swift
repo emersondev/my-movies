1f3d598bbf15876c797a5896668b9937ab90d06a
Emerson Novais <emersonnovais93@gmail.com>
2022-04-02 23:53:32 -0300

    Custom layout cell
    Custom layout

---
+++ my-movies/CellMovie.swift
@@ -0,0 +1,17 @@
+//
+//  CellMovie.swift
+//  my-movies
+//
+//  Created by Emerson.Novais on 02/04/22.
+//
+
+import UIKit
+
+class CellMovie : UITableViewCell {
+    
+    
+    
+    
+    
+    
+}
